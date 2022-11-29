b549cdc959d95c153c01f4e4d8cd233ca15a2de7
William Beemer <37132465+MrBubbles06@users.noreply.github.com>
2022-11-28 21:22:37 -0800

    added so much stuff (i know this is a bad message)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 const currentyeararabic = new Date().getFullYear();
-const copyrighttext = document.getElementById("currentyear");
-const hellodiv = document.querySelector(".hello");
 const currentyearroman = toRoman(currentyeararabic);
+
 const skills = ["javascript", "typescript", "html", "css", "python", "java"];
 const tools = ["firefox", "vs code", "git", "github", "zsh", "bash", "linux"];
 let toolcounter = 0;
 let skillcounter = 0;
+
+const copyrighttext = document.getElementById("currentyear");
+const hellodiv = document.querySelector(".hello");
+const languagediv = document.getElementsByClassName("sect1")[0];
+const toolsdiv = document.getElementsByClassName("sect2")[0];
 
 copyrighttext.innerHTML = currentyearroman;
 
@@ -19,16 +23,46 @@ copyrighttext.addEventListener("click", () => {
 
 setInterval(() => {
   document.querySelector(".sect1 > h2").innerHTML = skills[skillcounter];
-  document.querySelector(".sect2 > h2").innerHTML = tools[toolcounter];
   skillcounter++;
-  toolcounter++;
   if (skillcounter == 6) {
     skillcounter = 0;
   }
-  if (toolcounter == 7) {
-    toolcounter = 0;
+}, 1500);
+
+setTimeout(() => {
+  setInterval(() => {
+    document.querySelector(".sect2 > h2").innerHTML = tools[toolcounter];
+    toolcounter++;
+    if (toolcounter == 7) {
+      toolcounter = 0;
+    }
+  }, 1500);
+}, 750);
+
+let togglestate;
+languagediv.addEventListener("click", () => {
+  let oppositestate = document.querySelector(".sect2 > .more").className == "more unhid";
+  if (!togglestate || oppositestate) {
+    document.querySelector(".sect1 > .more").className = "more unhid";
+    document.querySelector(".sect2 > .more").className = "more";
+    togglestate = true;
+  } else {
+    document.querySelector(".sect1 > .more").className = "more";
+    togglestate = false;
   }
-}, 500);
+});
+
+toolsdiv.addEventListener("click", () => {
+  let oppositestate = document.querySelector(".sect1 > .more").className == "more unhid";
+  if (!togglestate || oppositestate) {
+    document.querySelector(".sect2 > .more").className = "more unhid";
+    document.querySelector(".sect1 > .more").className = "more";
+    togglestate = true;
+  } else {
+    document.querySelector(".sect2 > .more").className = "more";
+    togglestate = false;
+  }
+});
 
 function toRoman(num) {
   var listOfNum = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
